213984cd3ddb7c8c9d479ea684fca0749cf80db4
Alexfpg1976 <alexfpg@hotmail.com>
2024-07-16 21:22:18 -0500

    actualizacion indext.html

--- Encriptador.js
+++ Encriptador.js
@@ -1,21 +1,21 @@
 function encryptarTexto() {
     let inputText = document.getElementById("inputText").value;
-    let encryptedText = inputText
+    let encryptarTexto = inputText
         .replace(/e/g, "enter")
         .replace(/i/g, "imes")
         .replace(/a/g, "ai")
         .replace(/o/g, "ober")
         .replace(/u/g, "ufat");
-    document.getElementById("outputText").value = encryptedText;
+    document.getElementById("outputText").value = encryptarTexto;
 }
 
 function desencryptarTexto() {
     let inputText = document.getElementById("inputText").value;
-    let decryptedText = inputText
+    let desencryptarTexto = inputText
         .replace(/enter/g, "e")
         .replace(/imes/g, "i")
         .replace(/ai/g, "a")
         .replace(/ober/g, "o")
         .replace(/ufat/g, "u");
-    document.getElementById("outputText").value = decryptedText;
+    document.getElementById("outputText").value = desencryptarTexto;
 }
