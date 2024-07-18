bcfb0dfd14c92cfc398ddc385f80ce0604ac9dc3
Alexfpg1976 <alexfpg@hotmail.com>
2024-07-18 18:56:34 -0500

    actualizacion

--- Encriptador.js
+++ Encriptador.js
@@ -1,21 +1,30 @@
-function encryptarTexto() {
-    let inputText = document.getElementById("inputText").value;
-    let encryptarTexto = inputText
+function encrytarTexto() {
+    let text = document.getElementById("inputText").value;
+    let encryptedText = text
         .replace(/e/g, "enter")
         .replace(/i/g, "imes")
         .replace(/a/g, "ai")
         .replace(/o/g, "ober")
         .replace(/u/g, "ufat");
-    document.getElementById("outputText").value = encryptarTexto;
+    document.getElementById("outputText").value = encryptedText;
+    document.getElementById("inputText").value = "";
 }
 
 function desencryptarTexto() {
-    let inputText = document.getElementById("inputText").value;
-    let desencryptarTexto = inputText
+    let text = document.getElementById("inputText").value;
+    let decryptedText = text
         .replace(/enter/g, "e")
         .replace(/imes/g, "i")
         .replace(/ai/g, "a")
         .replace(/ober/g, "o")
         .replace(/ufat/g, "u");
-    document.getElementById("outputText").value = desencryptarTexto;
+    document.getElementById("outputText").value = decryptedText;
+    document.getElementById("inputText").value = "";
+}
+
+function copiarTexto() {
+    let outputText = document.getElementById("outputText");
+    outputText.select();
+    document.execCommand("copy");
+    //alert("Texto copiado al portapapeles");
 }
