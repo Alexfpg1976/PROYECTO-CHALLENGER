45a74550e9591f92aa4719b2596b1d80cc7c1f73
Alexfpg1976 <alexfpg@hotmail.com>
2024-07-21 23:57:21 -0500

    actualizacion programacion

--- Encriptador.js
+++ Encriptador.js
@@ -1,31 +1,52 @@
 function encrytarTexto() {
-    let text = document.getElementById("inputText").value;
+    let text = document.getElementById("introducirTexto").value;
     let encryptedText = text
         .replace(/e/g, "enter")
         .replace(/i/g, "imes")
         .replace(/a/g, "ai")
         .replace(/o/g, "ober")
         .replace(/u/g, "ufat");
-    document.getElementById("outputText").value = encryptedText;
-    document.getElementById("inputText").value = "";
+    document.getElementById("SalidaTexto").value = encryptedText;
+    document.getElementById("introducirTexto").value = "";
+    ocultarImagen();
+    return;
 }    
-
 function desencryptarTexto() {
-    let text = document.getElementById("inputText").value;
+    let text = document.getElementById("introducirTexto").value;
     let decryptedText = text
         .replace(/enter/g, "e")
         .replace(/imes/g, "i")
         .replace(/ai/g, "a")
         .replace(/ober/g, "o")
         .replace(/ufat/g, "u");
-    document.getElementById("outputText").value = decryptedText;
-    document.getElementById("inputText").value = "";
+    document.getElementById("SalidaTexto").value = decryptedText;
+    document.getElementById("introducirTexto").value = "";
+    ocultarImagen();
+    return;
    
 }
-
 function copiarTexto() {
-    let outputText = document.getElementById("outputText");
+    let outputText = document.getElementById("SalidaTexto");
     outputText.select();
     document.execCommand("copy");
+    limpiar();
+    PonerImagen();
     //alert("Texto copiado al portapapeles");
+    return;
+}
+function limpiar(){
+    document.querySelector('#SalidaTexto').value = '';
+    return;
+}
+function ocultarImagen() {
+    let imagen = document.getElementById("Muñeco");
+    if (imagen) {
+        imagen.style.display = 'none';
+    }
+}
+function PonerImagen() {
+    let imagen = document.getElementById("Muñeco");
+    if (imagen) {
+        imagen.style.display = 'block';
+    }
 }
