8eaf431738106a9c5c580abf156350a5d35c7c31
Alexfpg1976 <alexfpg@hotmail.com>
2024-08-01 14:29:38 -0500

    actualizaciones y arreglos

--- Encriptador.js
+++ Encriptador.js
@@ -1,53 +1,46 @@
-function encrytarTexto() {
-    let text = document.getElementById("introducirTexto").value;
-    let encryptedText = text
-        .replace(/e/g, "enter")
-        .replace(/i/g, "imes")
-        .replace(/a/g, "ai")
-        .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
-    document.getElementById("SalidaTexto").value = encryptedText;
-    document.getElementById("introducirTexto").value = "";
-    mostrarResultado();
-    return;
+const TextoEntrada = document.querySelector(".IntroducirTexto");
+const TextoSalida = document.querySelector(".SalidaTexto");
+
+function BotonEncriptar(){
+    const TextoEncriptado = Encriptar(TextoEntrada.value)
+    TextoSalida.value = TextoEncriptado 
+    TextoEntrada.value = "";
+    TextoSalida.style.backgroundImage = "none";                                                                      
 }
 
-function desencryptarTexto() {
-    let text = document.getElementById("introducirTexto").value;
-    let decryptedText = text
-        .replace(/enter/g, "e")
-        .replace(/imes/g, "i")
-        .replace(/ai/g, "a")
-        .replace(/ober/g, "o")
-        .replace(/ufat/g, "u");
-    document.getElementById("SalidaTexto").value = decryptedText;
-    document.getElementById("introducirTexto").value = "";
-    mostrarResultado();
-    return;
+function Encriptar(stringE){
+    let matriz = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringE = stringE.toLowerCase();
+
+    for(let i = 0; i < matriz.length; i++){
+        if(stringE.includes(matriz[i][0])){
+            stringE = stringE.replaceAll(matriz[i][0], matriz[i][1]);
+        }
+    }
+    return stringE;
 }
 
-function copiarTexto() {
-    let outputText = document.getElementById("SalidaTexto");
-    outputText.select();
+function BotonDesencriptar(){
+    const TextoEncriptado = Desencriptar(TextoEntrada.value);
+    TextoSalida.value = TextoEncriptado;
+    TextoEntrada.value = "";
+    TextoSalida.style.backgroundImage = "none";                                                                        
+}
+
+function Desencriptar(stringD){
+    let matriz = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringD = stringD.toLowerCase();
+
+    for(let i = 0; i < matriz.length; i++){
+        if(stringD.includes(matriz[i][1])){
+            stringD = stringD.replaceAll(matriz[i][1], matriz[i][0]);
+        }
+    }
+    return stringD;
+}
+function BotonCopiar() {
+    TextoSalida.select();
     document.execCommand("copy");
-    limpiar();
-    ocultarResultado();
-    return;
-}
-
-function limpiar() {
-    document.querySelector('#SalidaTexto').value = '';
-    return;
-}
-
-function mostrarResultado() {
-    document.getElementById("SalidaTexto").style.display = 'block';
-    document.getElementById("BotonCopiar").style.display = 'block';
-    document.getElementById("Muñeco").style.display = 'none';
-}
-
-function ocultarResultado() {
-    document.getElementById("SalidaTexto").style.display = 'none';
-    document.getElementById("BotonCopiar").style.display = 'none';
-    document.getElementById("Muñeco").style.display = 'block';
+    TextoSalida.style.backgroundImage = "url('Muñeco.png')";
+    TextoSalida.value = "";
 }
